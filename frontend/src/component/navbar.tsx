848cf5d03e20b4993d9175e816a3b2971a3bc1ee
Ali Saleh <aliosaleh336@gmail.com>
2026-05-04 01:46:34 +0300

    edit navbar

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -98,6 +98,14 @@ export default function Navbar() {
                   <LogIn className="w-5 h-5" />
                   <span>Login</span>
                 </Link>
+                <Link
+                  to="/register"
+                  onClick={() => setMobileMenuOpen(false)}
+                  className="flex items-center gap-2 px-4 py-3 rounded-lg border border-[#2A6F97] text-[#2A6F97]"
+                >
+                  <LogIn className="w-5 h-5" />
+                  <span>Register</span>
+                </Link>
               </div>
             </div>
           )}
