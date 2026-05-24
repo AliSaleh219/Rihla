135ee74c63214ffcc1594c9e274c73d7cb813bde
Ali Saleh <aliosaleh336@gmail.com>
2026-05-24 13:33:45 +0300

    edit the app page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Footer from "./component/footer.tsx";
+import Governorates from "./pages/Governorates.tsx";
+import ScrollToTop from "./component/scrollToTop.tsx";
+import ChatBot from "./pages/ai.tsx";
+import TripDetails from "./pages/TripDetails.tsx";
+import BookingPage from "./pages/booking.tsx";
+import Recommendation from "./pages/Recomandation.tsx";
+import TripsMaker from "./pages/TripsMaker.tsx";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
+import Navbar from "./component/navbar.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <BrowserRouter>
+              <Navbar />
+              <ScrollToTop />
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/governorates" element={<Governorates />} />
+                  <Route path="/recommendation" element={<Recommendation />} />
+                  <Route path="/trips" element={<TripsMaker />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/chat" element={<ChatBot />} />
+                  <Route path="/trips/:id" element={<TripDetails/>}/>
+                  <Route path="/booking/:id" element={<BookingPage/>} />
+              </Routes>
+              <Footer />
+          </BrowserRouter>
+      </>
   )
 }
 
